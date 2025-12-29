f06db44952a895258d10909766599181c0e09302
liuk123 <liukai_0218@qq.com>
2025-12-29 10:49:08 +0800

    1

--- src-modules/demo2/views/home.vue
+++ src-modules/demo2/views/home.vue
@@ -1,8 +1,10 @@
 <script setup>
 import useCounterStore  from '../store/index.js'
 import useCounterStoreFrmework from '@/framework/store'
+import useDemoCountStore from '../../demo/store'
 const store = useCounterStore()
 const frameStore = useCounterStoreFrmework()
+const demoStore = useDemoCountStore()
 
 
 </script>
@@ -11,8 +13,10 @@ const frameStore = useCounterStoreFrmework()
     <h1>demo2</h1>
     <div>store:{{ store.count }}</div>
     <div>frameStore:{{ frameStore.count }}</div>
+    <div>demoStore:{{ demoStore.count }}</div>
     <div class="action">
-        <button @click="store.increment">increment</button>
-        <button @click="frameStore.increment">increment</button>
+        <button @click="store.increment">store increment</button>
+        <button @click="frameStore.increment">frameStore increment</button>
+        <button @click="demoStore.increment">demoStore increment</button>
     </div>
 </template>
